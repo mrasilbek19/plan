66b049e224272f6702c34243bfdc12f36ad714f8
mrasilbek19 <asilbek190303@gmail.com>
2026-04-21 08:53:29 +0900

    BRR: building express web server

---
+++ server.js
@@ -0,0 +1,36 @@
+console.log("Web Serverni Boshlash");
+const express = require("express");
+const app = express();
+const http = require("http");
+
+// 4ta bosqich bor 
+
+// 1: Kirish code
+
+app.use(express.static("public")); //brauserda qelgan har qanday requestga public folder ochiq 
+app.use(express.json()); //json formatdagi data objda ugirib beradi
+app.use(express.urlencoded({extended: true})); // yozmasak HTML FORMdan kelgan narsalarni qabul qilmaydi
+
+
+// 2: Sessionga boglik hozircha qilmaymiz
+
+// 3: View code
+
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4: Routing codes
+
+app.get("/hello", function(req, res){
+    res.send(`<h1>Hello World</h1>`);
+});
+
+app.get("/gift", function(req, res){
+    res.send(`<h1>You are in gift page</h1>`);
+});
+
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function(){
+    console.log(`The server is running successfully on port ${PORT}`);
+})

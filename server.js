5ca2c93325022ed610454420f90737728603f858
mrasilbek19 <asilbek190303@gmail.com>
2026-04-23 19:11:03 +0900

    figma small works

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 console.log("Web Serverni Boshlash");
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data)=>{
+    if(err){
+        console.log("ERROR:", err);
+    }
+    else{
+        user = JSON.parse(data);
+    }
+});
 
 // 4ta bosqich bor 
 
@@ -21,10 +33,12 @@ app.set("view engine", "ejs");
 
 // 4: Routing codes
 app.post("/create-item", (req,res) =>{
-    console.log(req);
-    res.json({test: "sucsess"})
-})
+   
+});
 
+app.get("/author", (req, res) =>{
+    res.render("author",{user: user});
+});
 app.get("/", function(req, res){
     res.render("harid");
 });

e2f5f67a2b19c6c8c36b8570f3f54a3d7c787398
mrasilbek19 <asilbek190303@gmail.com>
2026-09-05 19:05:57 +0900

    fix: integration of .env

--- server.js
+++ server.js
@@ -1,29 +1,31 @@
 const http = require("http");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const mongodb = require("mongodb")
 
 let db;
-const connectionString = "mongodb+srv://Alex1903:XmMMVUp0IP7fakll@cluster0.ed6u0nb.mongodb.net/Reja";
+const connectionString = process.env.MONGO_URL;
 
-mongodb.connect(connectionString,{
+mongodb.connect(connectionString, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }, (err, client) => {
-    if(err) console.log("Error on connection to Mongodb");
-    else{
-        
+    if (err) console.log("Error on connection to Mongodb");
+    else {
+
         console.log("Connection to Mongodb succeed");
 
         module.exports = client;
         const app = require("./app");
-        
+
         const server = http.createServer(app);
-        let PORT = 3000;
-        server.listen(PORT, function(){
-        console.log(
-            `The server is running successfully on port ${PORT}, http://localhost:${PORT}`
-        );
-});
+        let PORT = 3009;
+        server.listen(PORT, function () {
+            console.log(
+                `The server is running successfully on port ${PORT}, http://localhost:${PORT}`
+            );
+        });
     }
 });
 
